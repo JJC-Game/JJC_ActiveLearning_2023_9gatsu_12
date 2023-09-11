58d9b7d50d7af91a4fdf3f293fc4c0d6e81068ea
Sayaka31 <jjc.23160134@gmail.com>
2023-09-11 15:52:16 +0900

    テーブルの編集

--- Etc/Laravel/2023_09_11_000909_create_app_users_table.php
+++ Etc/Laravel/2023_09_11_000909_create_app_users_table.php
@@ -9,14 +9,20 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::create('app_users', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
 
+     public function up(): void
+     {
+         Schema::create('app_users', function (Blueprint $table) {
+             $table->id();
+             $table->string('name');
+             $table->integer('gacha_point');
+             $table->integer('has_chara_flag')->unsigned();
+             $table->timestamps();
+         });
+     }
+ 
+
+     
     /**
      * Reverse the migrations.
      */
